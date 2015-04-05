ffb925cd59de990ced3ac6fc7df1b19b72b247f7
Klindson <klindson@live.com>
2015-04-05 06:54:29 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,3 +1,9 @@
+echo # master >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Klindson/master.git
+git push -u origin master
 (function () {
     
     var fork = "motelbible";
